44d8f3ad9a41f02342fae03db0a3cef0dd9e7d61
Dois Koh <kohehd@gmail.com>
2021-06-17 11:41:54 +0800

    Minor adjustments and form added.

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,9 +18,14 @@ import donor_tamil from "../images/donor_TamilS.png";
 import donor_marieanne from "../images/donor_marieanne.png";
 import donor_KelvinL from "../images/donor_KelvinL.png";
 import donor_Asyraf from "../images/donor_Asyraf.png";
+
 import { TelephoneFill, EnvelopeFill } from "react-bootstrap-icons";
+import { useForm } from "react-hook-form";
 
 export default function Layout({ children }) {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log( data );
+
   return (
     <div class="outer-div">
       <Container fluid>
@@ -41,7 +46,7 @@ export default function Layout({ children }) {
           </div>
         </Navbar>
 
-        <div>
+        <div class="sec-banner">
           <div class="hero-carousel w-100">
             <Carousel controls={false} indicators={false}>
               <Carousel.Item>
@@ -62,8 +67,8 @@ export default function Layout({ children }) {
                   src={splash2}
                   alt="Second slide"
                 />
-                <div class="splash twoguy">
-                  <span class="px-3">Splash 2 Guy.</span>
+                <div class="splash vignesh">
+                  <span class="px-3">Vignesh, 33. Sales Manager.</span>
                   <br />
                   <span class="px-3">A marrow donor.</span>
                 </div>
@@ -74,8 +79,8 @@ export default function Layout({ children }) {
                   src={splash3}
                   alt="Third slide"
                 />
-                <div class="splash threeguy">
-                  <span class="px-3">Splash 3 Guy.</span>
+                <div class="splash joshua">
+                  <span class="px-3">Joshua, 38. Senior Engineer.</span>
                   <br />
                   <span class="px-3">A marrow donor.</span>
                 </div>
@@ -94,19 +99,38 @@ export default function Layout({ children }) {
               </Carousel.Item>
             </Carousel>
           </div>
-          <div class="join-the-register">
-              <span class="px-3">Join the register.</span><br />
-              <span class="px-3">Help us find a donor</span><br />
+          <div class="sec-registration">
+            <div class="join-the-register">
+              <span class="px-3">Join the register.</span>
+              <br />
+              <span class="px-3">Help us find a donor</span>
+              <br />
               <span class="px-3">for every patient.</span>
+            </div>
+            <div class="registration-form d-flex flex-column">
+              <h1>Choose to save a life.</h1>
+              <form onSubmit={handleSubmit(onSubmit)} class="d-flex flex-column">
+                <input type="text" placeholder="Name"></input>
+                <input type="text" placeholder="Email"></input>
+                <input type="text" placeholder="Phone Number"></input>
+                <button>Submit</button>
+                <p>
+                  Your details will remain private and confidential. We conduct
+                  rigourous screening to make sure that those who qualify meet
+                  our health and safety standards. If selected, you will be
+                  required to perform a cheek swab before you are officially
+                  admitted to the register!
+                </p>
+              </form>
+            </div>
           </div>
         </div>
         <div class="sec-info bg-bmdp-2 py-5 d-flex flex-column align-items-center">
           <div class="sec-counter pt-2 pb-4">
             <div class="registrant-counter">112,300</div>
             <div>
-              Registrants (in Singapore) and counting. Join the cause and make
-              the community stronger, because ultimately, you too may benefit
-              from a larger marrow donor pool.
+              Registrants in Singapore and counting. Join the cause and make the
+              community stronger. Give hope to patients in need.
             </div>
           </div>
           <div class="sec-info-text py-5">
@@ -121,9 +145,9 @@ export default function Layout({ children }) {
           </div>
           <div class="container sec-info-cards">
             <div class="row px-5">
-              <div class="col-3 px-5">
+              <div class="col-6 col-md-3 px-5">
                 <div class="row justify-content-center">
-                  <img className="" src={touch_icon} />
+                  <img className="img-fluid" src={touch_icon} />
                 </div>
                 <div class="row justify-content-center mt-2">
                   <h3>Verify</h3>
@@ -133,9 +157,9 @@ export default function Layout({ children }) {
                   blood sample from you to ensure you are the right fit.
                 </div>
               </div>
-              <div class="col-3 px-5">
+              <div class="col-6 col-md-3 px-5">
                 <div class="row justify-content-center">
-                  <img className="" src={workup_icon} />
+                  <img className="img-fluid" src={workup_icon} />
                 </div>
                 <div class="row justify-content-center mt-2">
                   <h3>Workup</h3>
@@ -145,30 +169,30 @@ export default function Layout({ children }) {
                   thorough check-up to ensure that you are fit to donate.
                 </div>
               </div>
-              <div class="col-3 px-5">
+              <div class="col-6 col-md-3 px-5">
                 <div class="row justify-content-center">
-                  <img className="" src={give_icon} />
+                  <img className="img-fluid" src={give_icon} />
                 </div>
                 <div class="row justify-content-center mt-2">
                   <h3>Donate</h3>
                 </div>
                 <div class="row justify-content-center">
                   You may opt for the Peripheral Blood Stem Cell or Bone Marrow
-                  Collection. The method of collection may depend on the
+                  Collection. The donation method may also depend on the
                   patient’s needs.
                 </div>
               </div>
-              <div class="col-3 px-5">
+              <div class="col-6 col-md-3 px-5">
                 <div class="row justify-content-center">
-                  <img className="" src={recover_icon} />
+                  <img className="img-fluid" src={recover_icon} />
                 </div>
                 <div class="row justify-content-center mt-2">
                   <h3>Recover</h3>
                 </div>
                 <div class="row justify-content-center">
-                  You may experience side effects commonly associated with the
-                  flu. Your stem cells/bone marrow will regenerate inside 6
-                  weeks.
+                  Depending on the donation method, you may experience
+                  short-term side effects. Your stem cells/bone marrow will
+                  regenerate within 6 weeks.
                 </div>
               </div>
             </div>
@@ -186,18 +210,21 @@ export default function Layout({ children }) {
         </div>
         <div class="donor-testimonials py-5">
           <h1 class="text-center pb-4 bmdp-orange">Donor Testimonials</h1>
-          <Carousel>
+          <Carousel indicators={false}>
             <Carousel.Item>
               <div class="d-flex flex-row justify-content-center">
-                <div class=" d-flex flex-column justify-content-end align-items-end w-25">
+                <div class=" d-flex flex-column justify-content-start align-items-end w-25">
                   <div class="outer-div">
-                    <img class="donor-avatar p-2" src={donor_tamil}></img>
+                    <a href="https://bmdp.org/testimonials" target="_blank">
+                      {" "}
+                      <img class="donor-avatar p-2 img-fluid" src={donor_tamil}></img>
+                    </a>
                   </div>
                 </div>
                 <div class="testimonial-text d-flex flex-column justify-content-left w-50">
                   <h3 class="pl-5 pt-4">Tamil Selvi</h3>
                   <p class="pl-5">
-                    “The donation experience through the PBSV method was
+                    “The donation experience through the PBSC method was
                     worthwhile. It even motivated me to create more awareness
                     within the community to help improve the chances of other
                     patients in finding a donor match.”
@@ -207,59 +234,77 @@ export default function Layout({ children }) {
             </Carousel.Item>
             <Carousel.Item>
               <div class="d-flex flex-row justify-content-center">
-                <div class=" d-flex flex-column justify-content-end align-items-end w-25">
+                <div class=" d-flex flex-column justify-content-start align-items-end w-25">
                   <div class="outer-div">
-                    <img class="donor-avatar p-2" src={donor_marieanne}></img>
+                    <a
+                      href="https://www.youtube.com/watch?v=zHv24sJ-Emw"
+                      target="_blank"
+                    >
+                      <img class="donor-avatar p-2 img-fluid" src={donor_marieanne}></img>
+                    </a>
                   </div>
                 </div>
                 <div class="testimonial-text d-flex flex-column justify-content-left w-50">
-                  <h3 class="pl-5 pt-4">Marieanne</h3>
+                  <h3 class="pl-5 pt-4">Marie Ann Wong</h3>
                   <p class="pl-5">
-                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nulla aliquet ornare dignissim. Duis ac fringilla dolor.
-                    Vivamus vestibulum urna fringilla egestas convallis.”
+                    “I would urge everyone to put their fears aside and be
+                    confident as donating bone marrow is a safe and easy
+                    procedure with no long-term side effects. A simple cheek
+                    swab can help increase the odds of giving someone out there
+                    in need with a second chance at life.”
                   </p>
                 </div>
               </div>
             </Carousel.Item>
             <Carousel.Item>
               <div class="d-flex flex-row justify-content-center">
-                <div class=" d-flex flex-column justify-content-end align-items-end w-25">
+                <div class=" d-flex flex-column justify-content-start align-items-end w-25">
                   <div class="outer-div">
-                    <img class="donor-avatar p-2" src={donor_KelvinL}></img>
+                    <a
+                      href="https://www.youtube.com/watch?v=aX5j0QVHqB4"
+                      target="_blank"
+                    >
+                      <img class="donor-avatar p-2 img-fluid" src={donor_KelvinL}></img>
+                    </a>
                   </div>
                 </div>
                 <div class="testimonial-text d-flex flex-column justify-content-left w-50">
-                  <h3 class="pl-5 pt-4">Kelvin</h3>
+                  <h3 class="pl-5 pt-4">Kelvin Lin</h3>
                   <p class="pl-5">
-                    “Sed sit amet dolor ut sapien consequat imperdiet in vitae
-                    ex. Cras dignissim lorem orci, eget interdum odio mattis
-                    non. Sed faucibus cursus nisl, sit amet ultrices dui
-                    fermentum vel.”
+                    “I went through the PBSC method and the process is safe.
+                    Being a father now makes me feel that the whole donation was
+                    more meaningful. My biggest reward is having the opportunity
+                    to save someone’s life.”
                   </p>
                 </div>
               </div>
             </Carousel.Item>
             <Carousel.Item>
               <div class="d-flex flex-row justify-content-center">
-                <div class=" d-flex flex-column justify-content-end align-items-end w-25">
+                <div class=" d-flex flex-column justify-content-start align-items-end w-25">
                   <div class="outer-div">
-                    <img class="donor-avatar p-2" src={donor_Asyraf}></img>
+                    <a
+                      href="https://www.youtube.com/watch?v=6RbaPEEKRvg"
+                      target="_blank"
+                    >
+                      <img class="donor-avatar p-2 img-fluid" src={donor_Asyraf}></img>
+                    </a>
                   </div>
                 </div>
                 <div class="testimonial-text d-flex flex-column justify-content-left w-50">
-                  <h3 class="pl-5 pt-4">Asyraf</h3>
+                  <h3 class="pl-5 pt-4">Muhammad Asyraf</h3>
                   <p class="pl-5">
-                    “Donec ligula erat, vehicula vel turpis ac, congue faucibus
-                    nulla. Suspendisse at enim id lorem porta iaculis. Nunc
-                    lorem magna, porttitor at eleifend vel, feugiat eget quam.”
+                    “Educate & understand why and who you’re helping so you will
+                    remain committed in your journey after you sign up. I was
+                    happy that I’m able to help someone and I would do it again
+                    without second thoughts.”
                   </p>
                 </div>
               </div>
             </Carousel.Item>
           </Carousel>
         </div>
-        <div class="footer p-4 d-flex flex-row justify-content-between">
+        <div class="footer p-4 d-flex flex-row justify-content-between align-items-end">
           <div class="ml-5">
             <img src={logo_orange}></img>
           </div>
